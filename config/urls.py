becfbdd0f5e2b3b74eed56dbcf78c7148faa20a5
okdtr <okada.teru@gmail.com>
2020-03-06 17:03:16 +0800

    tweeked config/urls

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('apiv1.urls')),
+    re_path(RedirectView.as_view(url='/')),
 ]
