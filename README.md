b11b842f3aaf96951ae426e2373e6a2a2cde9ba7
okdtr <okada.teru@gmail.com>
2020-03-06 16:56:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# django_API_vue_SPA-
---
+++ drf-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/okada/Documents/work/python_project/drf-simplest-sample/drf-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
